c7ab6a7f80ca52b41b8f6c030b944de654eba1cf
martincou29 <martincoutinho00@gmail.com>
2021-01-19 10:25:06 -0300

    path finding

---
+++ Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -0,0 +1,25 @@
+﻿using SimplePlatformer.Enemy;
+using UnityEngine;
+using Pathfinding;
+
+public class FlyingEnemy : Enemy
+{
+    private AIPath aiPath;
+
+    private void Awake()
+    {
+        aiPath = FindObjectOfType<AIPath>().GetComponent<AIPath>();
+    }
+
+    protected override void Move()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f,1f);
+        }else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f,1f);
+
+        }
+    }
+}
