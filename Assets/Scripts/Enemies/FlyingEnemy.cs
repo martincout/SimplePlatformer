882e83f94de56eca4c9b9930f5a659da3fa54993
martincou29 <martincoutinho00@gmail.com>
2021-01-19 21:31:34 -0300

    Refractoring code

--- Assets/Scripts/Enemies/FlyingEnemy.cs
+++ Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -15,8 +15,7 @@ public class FlyingEnemy : Enemy
     {
         if (seeker.IsDone())
         {
-        seeker.StartPath(rb2d.position, target.position, OnPathComplete);
-
+            seeker.StartPath(rb2d.position, target.position, OnPathComplete);
         }
 
     }
@@ -37,8 +36,10 @@ public class FlyingEnemy : Enemy
 
     protected override void FixedUpdate()
     {
+        //No path
         if (path == null)
             return;
+        //Check reached end of path
         if (currentWaypoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = false;
@@ -48,14 +49,30 @@ public class FlyingEnemy : Enemy
         {
             reachedEndOfPath = true;
         }
-
+        //Direction and Movement
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb2d.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
         rb2d.AddForce(force);
+        //Distance
         float distance = Vector2.Distance(rb2d.position, path.vectorPath[currentWaypoint]);
-        if(distance < nextWaypointDistance)
+
+        if (distance < nextWaypointDistance)
         {
             currentWaypoint++;
+        }
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (rb2d.velocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (rb2d.velocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
         }
     }
     protected override void Update()
