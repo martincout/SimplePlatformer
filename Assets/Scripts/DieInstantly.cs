3a12b00d67b61fdb27f2044f1efa9d3bde9e90eb
martincou29 <martincoutinho00@gmail.com>
2021-01-24 23:02:59 -0300

    It doesn't inheritance from DoDamage now

--- Assets/Scripts/DieInstantly.cs
+++ Assets/Scripts/DieInstantly.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DieInstantly : DoDamage
+public class DieInstantly : MonoBehaviour
 {
-    protected  override void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<IDamagable>() != null)
         {
