6a23234d754f454ead804949c45d51e78d512c26
martincou29 <martincoutinho00@gmail.com>
2021-01-27 22:59:32 -0300

    lines

--- Assets/Scripts/Menus/DeathMenu.cs
+++ Assets/Scripts/Menus/DeathMenu.cs
@@ -19,10 +19,7 @@ public class DeathMenu : MonoBehaviour
     private IEnumerator Fade()
     {
         CanvasGroup canvasGroup = deathScreenGO.GetComponent<CanvasGroup>();
-
         canvasGroup.alpha = 0f;
-
-
         float duration = 1f;
         LeanTween.alphaCanvas(canvasGroup, 1.0f, duration);
         yield return new WaitForSeconds(duration);
