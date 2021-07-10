1958e62df8cc31907d0880422b2f2639b0d459f6
martincou29 <martincoutinho00@gmail.com>
2021-07-10 20:31:52 -0300

    boss implemented, basic attack with hitbox OnTriggerEnter2d

--- Assets/Scripts/Enemies/BossBehaviour.cs
+++ Assets/Scripts/Enemies/BossBehaviour.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 namespace SimplePlatformer.Enemy
 {
+    /// <summary>
+    /// Enemy boss behaviour class, simplified the structure of the Enemy Behaviour
+    /// </summary>
     public class BossBehaviour : MonoBehaviour
     {
         [Expandable]
@@ -13,13 +16,24 @@ namespace SimplePlatformer.Enemy
         protected HealthSystem healthSystem;
         private Rigidbody2D rb2d;
         private Animator anim;
-        private GameObject playerGO;
+        /// <summary>
+        /// Ground Detection
+        /// </summary>
+        public Transform groundDetector;
+        private RaycastHit2D raycastGround;
+        [SerializeField] private float rayLength = 0.2f;
+        /// <summary>
+        /// Player
+        /// </summary>
+        private Vector2 playerPosition;
+        [SerializeField] private GameObject playerGO;
+
+
+        private float cooldownAttack = 0;
 
         /// <summary>
-        /// Movement
+        /// Finite State Machine
         /// </summary>
-        
-
         private enum State
         {
             IDLE,
@@ -29,6 +43,9 @@ namespace SimplePlatformer.Enemy
         }
         private State _currentState;
 
+        /// <summary>
+        /// Boss Phases
+        /// </summary>
         private enum Phase
         {
             FIRST,
@@ -37,8 +54,10 @@ namespace SimplePlatformer.Enemy
         private Phase _currentPhase;
 
         /// <summary>
-        /// Custom Behaviours of the Enemy
+        /// Custom Behaviours
         /// </summary>
+        private bool isAttacking;
+        private float countBasicAttacks = 0;
 
         private void Start()
         {
@@ -52,24 +71,51 @@ namespace SimplePlatformer.Enemy
 
         private void Update()
         {
+            CooldownAttack();
             Movement();
         }
 
+        /// <summary>
+        /// Handles all movement behaviour of the boss
+        /// </summary>
         private void Movement()
         {
             CheckPlayer();
             Move();
+
         }
 
         private void Move()
         {
-            
+            if (!isAttacking)
+            {
+                FlipByTargetDirection(playerPosition.x);
+                if (playerPosition.x != 0 && CheckGround())
+                {
+                    anim.Play(_bossData.animation.enemyMovement);
+                    rb2d.velocity = new Vector2(playerPosition.x * _bossData.speed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+                }
+                
+                //Idle animation only if we have no ground, because otherwise the enemy will keep walking when it's not suppose to do.
+                //Not play idle animation when attacking because it will cancel the attack animation
+                if (!CheckGround())
+                {
+                    anim.Play(_bossData.animation.enemyIdle);
+                    rb2d.velocity = Vector2.zero;
+                }
+
+            }
+            else
+            {
+                //Stops moving when attacking
+                rb2d.velocity = Vector2.zero;
+            }
         }
 
         private void CheckPlayer()
         {
             Collider2D[] boxAttackRadius = Physics2D.OverlapBoxAll(transform.position, _bossData.attackRadius, 0, 1 << LayerMask.NameToLayer("Player"));
-            Vector3 dir = (playerGO.transform.position - transform.position).normalized;
+            playerPosition = (playerGO.transform.position - transform.position).normalized;
 
             //Check if there is a player in the attack radius
             if (boxAttackRadius.Length > 0)
@@ -87,8 +133,74 @@ namespace SimplePlatformer.Enemy
 
         private void Attack()
         {
-
+            if (!isAttacking)
+            {
+                StartCoroutine(CooldownAttack());
+                isAttacking = true;
+                anim.Play(_bossData.animation.enemyAttack[0], -1,0f);
+            }
         }
-    }
+
+   
+
+        protected bool CheckGround()
+        {
+            //float rayLength = Mathf.Abs(deltaMove.x) + skinWidth * 2;
+
+            //Vector2 rayOrigin = (directionX == -1 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) +
+            //    Vector2.up * deltaMove.y;
+            Vector2 rayOrigin = groundDetector.position;
+            raycastGround = Physics2D.Raycast(rayOrigin, Vector2.down, rayLength, 1 << LayerMask.NameToLayer("Ground"));
+
+            if (raycastGround)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(groundDetector.position, Vector2.down * rayLength);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, _bossData.attackRadius);
+            Gizmos.color = Color.yellow;
+            if (playerGO != null)
+            {
+                Gizmos.DrawLine(transform.position, playerGO.transform.position);
+            }
+        }
+
+        private IEnumerator CooldownAttack()
+        {
+            yield return new WaitForSeconds(_bossData.attackRate);
+            //Cooldown Attack
+            isAttacking = false;
+            FlipByTargetDirection(playerPosition.x);
+        }
+
+        /// <summary>
+        /// Utils
+        /// </summary>
+        public void FlipByTargetDirection(float _dirX)
+        {
+            if (_dirX > 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (_dirX < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        public void SwishSound()
+        {
+            SoundManager.instance.Play(_bossData.swishSound);
+        }
+    }//End class
 }
 
---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public string targetTag;
+    public float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(targetTag))
+        {
+            if (collision.GetComponent<IDamageable>() != null)
+            {
+                collision.GetComponent<IDamageable>().TakeDamage(damage,transform.position);
+            }
+        }
+    }
+}
