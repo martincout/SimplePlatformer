41d16a416b936cdf5a35d2494efd5f9edd3c3fb1
martincou29 <martincoutinho00@gmail.com>
2021-09-17 18:13:56 -0300

    Loading animation with unscaled coroutines and animation

---
+++ Assets/loadingGif.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingGif : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(wait());
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        this.gameObject.SetActive(false);
+    }
+
+}
