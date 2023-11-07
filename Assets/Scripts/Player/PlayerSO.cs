2058f736db38370f10bf8d8fb962ff2d83ba091a
Martin Coutinho <martincoutinho00@gmail.com>
2023-11-07 11:58:43 -0300

    Player Scriptable object, added more props

--- Assets/Scripts/Player/PlayerSO.cs
+++ Assets/Scripts/Player/PlayerSO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimplePlatformer.Player;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace SimplePlatformer.Assets.Scripts.Player
 {
     public class PlayerSO : ScriptableObject
     {
+        [Header("Movement")]
         [Tooltip("The maximum speed that can perform gradually.")]
         public float MaxSpeed = 10f;
         [Tooltip("The acceleration is how fast will the object reach the maximum speed.")]
@@ -23,5 +25,35 @@ namespace SimplePlatformer.Assets.Scripts.Player
         public float JumpForce = 4f;
         [Tooltip("Hang time in the air (Coyote time)")] 
         public float HangTime = 0.2f;
+        [Header("Ground")]
+        [Tooltip("Ground raycast height.")]
+        public float groundedHeight = 0.5f;
+        [Tooltip("Ground raycast offset.")]
+        public float heightOffset = 0.25f; // we dont want to cast from the players feet (may cast underground sometimes), so we offset it a bit
+        [Tooltip("Ground Layer")]
+        public LayerMask groundLayer;
+        [Tooltip("Left Raycast Offset")]
+        public Vector3 raycastLeftOffset = new Vector2(0.5f, 0);
+        [Tooltip("Right Raycast Offset")]
+        public Vector3 raycastRightOffset = new Vector2(0.5f, 0);
+        [Header("Combat")]
+        [Tooltip("Attack cooldown for the next attack")] 
+        public float AttackRate = 0.3f;
+        [Tooltip("The damage")] 
+        public float AttackDamage = 10f;
+        [Tooltip("Initial friction of the character.")] 
+        public float InitialDrag;
+        [Tooltip("Friction when attacking.")] 
+        public float AttackDrag;
+        [Tooltip("Arrow Prefab")] 
+        public GameObject PF_Arrow;
+        [Header("HitBox")]
+        public Vector3 HitboxSize;
+        public float Rotation;
+        [Tooltip("Time to perform the next combo attack.")]
+        public float TimeNextCombo = 0.3f;
+        [Header("Particles")]
+        public GameObject PF_HurtParticle;
+        public GameObject PF_FootstepsParticle;
     }
 }
